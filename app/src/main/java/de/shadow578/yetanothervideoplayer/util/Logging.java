3b58a36d2b562c7119b664439016794cb93e62a1
shadow578 <christophnienhaus2@gmail.com>
2019-08-24 13:19:47 +0200

    Add Logging Util Class

---
+++ app/src/main/java/de/shadow578/yetanothervideoplayer/util/Logging.java
@@ -0,0 +1,46 @@
+package de.shadow578.yetanothervideoplayer.util;
+
+import android.util.Log;
+
+import java.util.Locale;
+
+public final class Logging
+{
+    /**
+     * The tag used for log messages
+     */
+    static final String TAG = "YAVP";
+
+    /**
+     * Log a Debug/Verbose message
+     *
+     * @param msg    the message to log
+     * @param format formatting options for the message
+     */
+    public static void logD(String msg, Object... format)
+    {
+        Log.d(TAG, String.format(Locale.US, msg, format));
+    }
+
+    /**
+     * Log a Warning message
+     *
+     * @param msg    the message to log
+     * @param format formatting options for the message
+     */
+    public static void logW(String msg, Object... format)
+    {
+        Log.w(TAG, String.format(Locale.US, msg, format));
+    }
+
+    /**
+     * Log a Error message
+     *
+     * @param msg    the message to log
+     * @param format formatting options for the message
+     */
+    public static void logE(String msg, Object... format)
+    {
+        Log.e(TAG, String.format(Locale.US, msg, format));
+    }
+}
