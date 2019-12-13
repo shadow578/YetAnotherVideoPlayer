2d833972d974f399920f94b887bf5c2dd57e2175
shadow578 <christophnienhaus2@gmail.com>
2019-12-13 23:30:51 +0100

    implement colorget shader

--- app/src/main/res/raw/colorget.fs
+++ app/src/main/res/raw/colorget.fs
@@ -6,11 +6,31 @@ varying highp vec2 vTextureCoord;
 //the current texture
 uniform lowp sampler2D sTexture;
 
+float getLuminance(vec4 c)
+{
+    float mx = c.r;
+    float mn = c.r;
+
+    if(c.g > mx) mx = c.g;
+    if(c.b > mx) mx = c.b;
+
+    if(c.g < mn) mn = c.g;
+    if(c.b < mn) mn = c.b;
+
+    return (mx + mn) / 2.0;
+}
+
 void main()
 {
     //get color on texture at current position
     vec4 c = texture2D(sTexture, vTextureCoord);
 
+    //calculate luminance
+    float pxLuminance = getLuminance(c);
+
+    //clamp to range 0 - 1
+    pxLuminance = clamp(pxLuminance, 0.0, 1.0);
+
     //set current fragment color
-    gl_FragColor = c.rgba;
+    gl_FragColor = vec4(c.rgb, pxLuminance);
 }
